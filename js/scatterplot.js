655567121654dd8ff2f3b8b8b9ab91e6c8469018
gaealeemon <92835945+gaealeemon@users.noreply.github.com>
2022-02-28 12:10:26 -0500

    added a scatterplot

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -8,6 +8,21 @@ Modified: 12/08/21
 
 // Build your scatterplot in this file 
 
+let svg2 = d3.select("body")
+            .append("svg")
+                .attr("class", "vis-holder");
+
+d3.csv("data/scatter.csv").then((data) => {
+	svg2.selectAll("circle")
+	.data(data)
+    .enter()
+    .append("circle")
+        .attr("cx", (d) => { return d.x; })
+        .attr("cy", (d) => { return d.y; })
+        .attr("r", 30)
+        .attr("fill", (d) => { return d.color; });
+}
+
 
 
 
